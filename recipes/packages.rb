eb7c600cc2ec6204197dbe4a988486c489a7425a
Alex Pop <al3xpop@gmail.com>
2014-11-10 15:04:42 +0000

    fix bash

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -7,7 +7,7 @@ include_recipe 'ohai::default'
 
 log "*** Bash vulnerable: #{node['languages']['bash']['shellshock_vulnerable']}"
 
-#package 'bash' do
-#  action :upgrade
-#  only_if { node['languages']['bash']['shellshock_vulnerable'] }
-#end
+package 'bash' do
+  action :upgrade
+  only_if { node['languages']['bash']['shellshock_vulnerable'] }
+end
